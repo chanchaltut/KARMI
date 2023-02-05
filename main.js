496cbe2f494f88fa5e124f0ca8c09d8a6f28ab34
Chanchal Pradhan <chanchalpradhan.coder@gmail.com>
2023-02-05 22:46:11 +0530

    Logo Added and Many More

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
 const navLists = document.querySelectorAll(".nav-list");
+const header = document.querySelector("header");
 const hambugerMenu = document.querySelector(".hamburger-menu");
+
+const aboutList = document.querySelector('.about-list');
+const DDMenu1 = document.querySelector('.dm-1');
+
+const worksList = document.querySelector('.works-list');
+const DDMenu2 = document.querySelector('.dm-2');
+
+let tabletSize = window.matchMedia("(max-width:1000px)");
+
+function clickEnable(tabletSize) {
+  if (tabletSize.matches) {
+    aboutList.addEventListener("click", () => {
+      DDMenu1.classList.toggle("d-flex");
+    });
+
+    worksList.addEventListener("click", () => {
+      DDMenu2.classList.toggle("d-flex");
+    });
+  } else {
+    null;
+  }
+};
+
+clickEnable(tabletSize);
 
 hambugerMenu.addEventListener("click", () => {
   header.classList.toggle("active");
@@ -7,8 +32,8 @@ hambugerMenu.addEventListener("click", () => {
 
 window.addEventListener("scroll", () => {
   document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
-    ? header.classList.add("sticky")
-    : header.classList.remove("sticky");
+    ? navLists.forEach((navList) => navList.classList.add("sticky"))
+    : navLists.forEach((navList) => navList.classList.remove("sticky"));
 });
 
 let swiper = new Swiper(".swipo", {
