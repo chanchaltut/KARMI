26a602450538b3b0c9c8ea61f8afbe2541e92e3f
coder-sam-007 <sameerjain.coder@gmail.com>
2023-02-20 10:59:39 +0530

    header changed

--- main.js
+++ main.js
@@ -1,45 +1,23 @@
+const menu_btn = document.querySelector(".menu-btn");
 const header = document.querySelector("header");
-const navLists = document.querySelectorAll(".nav-list");
-const DMLists = document.querySelectorAll(".dm-lists");
-const hambugerMenu = document.querySelector(".hamburger-menu");
-const ul = document.querySelector(".menu-list>ul");
+const dropdown = document.querySelectorAll(".dropdown");
 
 window.addEventListener("scroll", () => {
   document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
-    ? navLists.forEach((navList) => navList.classList.add("sticky"))
-    : navLists.forEach((navList) => navList.classList.remove("sticky"));
+    ? header.classList.add("sticky")
+    : header.classList.remove("sticky");
 });
 
-hambugerMenu.addEventListener("click", () => {
+menu_btn.addEventListener("click", (e) => {
   header.classList.toggle("active");
+  dropdown.forEach((e) => e.classList.toggle("hover"));
 });
 
-DMLists.forEach((DMlist) => {
-  DMlist.addEventListener(
-    "click",
-    () => DMlist.classList.toggle("dm-show"),
-    document.body.addEventListener("click", () =>
-      DMlist.classList.remove("dm-show")
-    )
-  );
-});
-
-ul.addEventListener(
-  "click",
-  function (ev) {
-    ev.stopPropagation();
-  },
-  false
-);
-
-DMLists[0].addEventListener("click", () => {
-  if (DMLists[1].classList.contains("dm-show"))
-    DMLists[1].classList.remove("dm-show");
-});
-
-DMLists[1].addEventListener("click", () => {
-  if (DMLists[0].classList.contains("dm-show"))
-    DMLists[0].classList.remove("dm-show");
+dropdown.forEach((d) => {
+  d.addEventListener("click", (e) => {
+    e.target.parentElement.querySelector(".sub-menu").classList.toggle("show");
+    d.querySelector("i").classList.toggle("rotate");
+  });
 });
 
 let swiper = new Swiper(".swipo", {
